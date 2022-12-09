1c4e32d702269905b65a1aad9403bc7c38c34e95
Linus-Alexander Gubenis <linus.gubenis@gmail.com>
2022-12-09 13:39:25 +0100

    infra(database): create primary auto_inc id and retain original_id
    I decided it would be a nicer API usage experience if we only retain the original_id as data and create a new unique primary auto_increment key for actual API usage, like this we don’t loose data (the original_id which has duplicates) but can reference to each pokemon specifically.

--- database/docker/mysql/3_import_seed-data.sql
+++ database/docker/mysql/3_import_seed-data.sql
@@ -4,6 +4,6 @@ LOAD DATA INFILE '/var/lib/mysql-files/pokemon.csv'
   ENCLOSED BY ''
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
-  (id, name, type1, type2, total, healthpoints, attack, defense, special_attack, special_defense, speed, generation, @legendary)
+  (original_id, name, type1, type2, total, healthpoints, attack, defense, special_attack, special_defense, speed, generation, @legendary)
   SET legendary = IF(@legendary = 'True', 1, 0);
   
