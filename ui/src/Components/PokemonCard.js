709b10e60328e078444c26d102d78ebceb7d6d25
Linus-Alexander Gubenis <linus.gubenis@gmail.com>
2022-12-09 21:12:50 +0100

    chore(eslint): apply prettier codestyle to UI code

--- ui/src/Components/PokemonCard.js
+++ ui/src/Components/PokemonCard.js
@@ -1,28 +1,49 @@
-import { Card, CardBody, CardHeader, Flex, GridItem, Image, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  GridItem,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-const SPRITE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+import { SPRITE_URL } from '../utils';
+
+const legendaryGradient = 'linear(to-br, yellow.50, yellow.200)';
+const commonGradient = 'linear(to-br, gray.50, white)';
 
 function PokemonCard({ pokemon }) {
+  const bgGradient = pokemon.legendary ? legendaryGradient : commonGradient;
 
-  const bgGradient = pokemon.legendary ? 'linear(to-br, yellow.50, yellow.200)' : 'linear(to-br, gray.50, white)';
-
-  return <GridItem>
-  <Link to={`/detail/${pokemon.id}`}>
-    <Card bgGradient={bgGradient}>
-      <CardHeader><Text maxWidth='100%' noOfLines={1}>{pokemon.name}</Text></CardHeader>
-      <CardBody>
-        <Flex justify="center" height={140}>
-          <Image height={24} src={`${SPRITE_URL}${pokemon.original_id}.png`} />
-        </Flex>
-        <Flex justify="space-between" p={0}>
-          <Text>#{pokemon.original_id}</Text>
-          <Text fontSize={16}>{pokemon.type1} {pokemon.type2 && '&'} {pokemon.type2}</Text>
-        </Flex>
-      </CardBody>
-    </Card>
-    </Link>
-  </GridItem>
+  return (
+    <GridItem>
+      <Link to={`/detail/${pokemon.id}`}>
+        <Card bgGradient={bgGradient}>
+          <CardHeader>
+            <Text maxWidth="100%" noOfLines={1}>
+              {pokemon.name}
+            </Text>
+          </CardHeader>
+          <CardBody>
+            <Flex justify="center" height={140}>
+              <Image
+                height={24}
+                src={`${SPRITE_URL}${pokemon.original_id}.png`}
+              />
+            </Flex>
+            <Flex justify="space-between" p={0}>
+              <Text>#{pokemon.original_id}</Text>
+              <Text fontSize={16}>
+                {pokemon.type1} {pokemon.type2 && '&'} {pokemon.type2}
+              </Text>
+            </Flex>
+          </CardBody>
+        </Card>
+      </Link>
+    </GridItem>
+  );
 }
 
 export default PokemonCard;
--- ui/src/test-utils.js
+++ ui/src/test-utils.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
-const AllProviders = ({ children }) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
-);
+function AllProviders({ children }) {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+}
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: AllProviders, ...options });
